639cad4e4a1b39bf7ad2f8b1e5ee00dcae25dfde
Alexander Decker <Deckeraga@alexanders-macbook-air.local>
2015-07-05 14:56:42 -0500

    Still trying to figure out how the coordinates work

--- Chess/GameScene.swift
+++ Chess/GameScene.swift
@@ -16,31 +16,11 @@ class GameScene: SKScene {
         
         let box = SKShapeNode(rectOfSize: CGSize(width: 50, height: 50))
         box.name = "bar"
-        box.fillColor = SKColor(red:1.00, green:0.00, blue:0.00, alpha:1)
-        box.position = CGPoint(x: 100, y: 100)
+        box.fillColor = SKColor(red:0.00, green:0.00, blue:0.00, alpha:1)
+        box.position = CGPoint(x: 0, y: 0)
         
         self.addChild(box)
         
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
